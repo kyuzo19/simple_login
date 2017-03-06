0dfdce84a972eb91b53c123c166a6ce04b90366c
kenneth araniador <kenara19@gmail.com>
2017-03-05 23:04:04 -0800

    recode

--- app.js
+++ app.js
@@ -10,11 +10,12 @@ var routes = require("./api/routes");
 app.set("port", (process.env.PORT || 5000));
 
 app.use(function (req, res, next) {
-    console.log(req.method, res.method);
+    console.log(req.method, req.url);
     next();
 
 });
 //serve angular app files to /public
+//middleware
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/node_modules", express.static(__dirname + "/node_modules"));
 
--- public/app.js
+++ public/app.js
@@ -1,117 +1,143 @@
-angular.module("loginApp", ["angular-jwt"])
-.config(function ($httpProvider){
-    $httpProvider.interceptors.push("authInterceptor");
-})
-.factory("authStatus", function  () {
- 	 var auth = {
-        isLoggedIn: false
-    };
-	return auth;
-	
-	
-})
-.factory("authInterceptor", ["$location", "$window", "authStatus", function  ($location, $window, authStatus) {
-	return {
-		request : request,
-		response : response,
-		responseError : responseError,
-		requestError : requestError
+(function () { 
+	angular
+		.module("loginApp", ["angular-jwt"])
+		.config(function ($httpProvider){
+			$httpProvider.interceptors.push("authInterceptor");
+		})
+		.factory("authStatus", authStatus)
+		.factory("authInterceptor", authInterceptor)
+		.controller("LoginCtrl", LoginCtrl)
+		.controller("LogoutCtrl", LogoutCtrl)
+		.controller("EmployeeCtrl", EmployeeCtrl)
+		
+	function authStatus () {
+ 			var auth = {
+      			isLoggedIn: false
+		};
+		return auth;		
 	};
 	
-	function request (config) {
-		config.headers = config.headers || {};
-        if ($window.sessionStorage.token) {
-            config.headers.Authorization = "Bearer " + $window.sessionStorage.token;
-        };
-        
-        return config;
-	};
+	authInterceptor.$interceptor = ["$location", "$window", "authStatus"];
 	
-	function response (response) {
-		if (response.status === 200 && $window.sessionStorage.token && !authStatus.isLoggedIn) {
-    		authStatus.isLoggedIn = true;
-        };
-		if (response.status === 401) {
-			authStatus.isLoggedIn = false;
+	function authInterceptor ($location, $window, authStatus){
+		return {
+			request : request,
+			response : response,
+			responseError : responseError,
+			requestError : requestError
 		};
 		
-		return response || $q.when(response);
-	};
-	
-	function requestError (rejection) {
+		function request (config) {
+			config.headers = config.headers || {};
+			if ($window.sessionStorage.token) {
+				config.headers.Authorization = "Bearer " + $window.sessionStorage.token;
+			};
+			
+			return config;
+		};
+		
+		function response (response) {
+			if (response.status === 200 && $window.sessionStorage.token && !authStatus.isLoggedIn) {
+				authStatus.isLoggedIn = true;
+			};
+			if (response.status === 401) {
+				authStatus.isLoggedIn = false;
+			};
+			
+			return response || $q.when(response);
+		};
+		
+		function requestError (rejection) {
 			if (rejection.status === 401 || rejection.status === 403) {
 				delete $window.sessionStorage.token;
 				authStatus.isLoggedIn = false;
 				$location.path("/");
 			}
+		};
+		
+		function responseError () {
+			
+		};
 	};
 	
-	function responseError () {
-		
-	};
-}])
-.controller("loginCtrl", ["$http", "jwtHelper", "$window", "authStatus", function ($http, jwtHelper, $window, authStatus){
-    var vm = this;
-	vm.isLoggedIn = function() {
-   		if (authStatus.isLoggedIn) {
-            return true;
-        } else {
-            return false;
-        }
-	};
-    
-    vm.login = function (){
-        
-        var data = {
-            username : vm.username,
-            password : vm.password
-        };
-        
-        $http.post("/users/login", data).then(function(res){
-            console.log(res);
-            
-            $window.sessionStorage.token = res.data.token;
-            var token = $window.sessionStorage.token;
-            var decodedToken = jwtHelper.decodeToken(token);
-            vm.username = decodedToken.username;
-			authStatus.isLoggedIn = true;
-            console.log("token" + decodedToken.username);
-        }).catch(function(err){
-            console.log(err);
-        });
-        
-    };
-}])
-.controller("logoutCtrl", ["$window", "$location", "authStatus", function ($window, $location, authStatus) {
-	var vm = this;
-	vm.isLoggedIn = function () {
+	LoginCtrl.$inject = ["$http", "jwtHelper", "$window", "authStatus"];
+
+	function LoginCtrl ($http, jwtHelper, $window, authStatus){
+		var vm = this;
+		vm.isLoggedIn = function() {
 			if (authStatus.isLoggedIn) {
-            	return true;
+				return true;
 			} else {
-            	return false;
+				return false;
 			}
 		};
-	vm.logout = function () {
-        
-        authStatus.isLoggedIn = false;
-        delete $window.sessionStorage.token;
-        $location.path("/");
-        
-    };
-}])
-.controller("employeeCtrl", ["$http", "authStatus", function ($http, authStatus) {
-   	var vm = this;
-	vm.isLoggedIn = function() {
-   		if (authStatus.isLoggedIn) {
-			return true;
-		} else {
-			return false;
+		
+		vm.login = function (){
+			
+			var data = {
+				username : vm.username,
+				password : vm.password
+			};
+			
+			$http.post("/users/login", data).then(function(res){
+				console.log(res);
+				
+				$window.sessionStorage.token = res.data.token;
+				var token = $window.sessionStorage.token;
+				var decodedToken = jwtHelper.decodeToken(token);
+				vm.username = decodedToken.username;
+				authStatus.isLoggedIn = true;
+				console.log("token" + decodedToken.username);
+			}).catch(function(err){
+				console.log(err);
+			});
+			
+		};
+		
+		
+	};
+	
+	LogoutCtrl.$inject = ["$window", "$location", "authStatus"]
+
+	function LogoutCtrl ($window, $location, authStatus) {
+		var vm = this;
+		vm.isLoggedIn = function () {
+			if (authStatus.isLoggedIn) {
+				return true;
+			} else {
+				return false;
+			}
+		};
+		vm.logout = function () {
+			
+			authStatus.isLoggedIn = false;
+			delete $window.sessionStorage.token;
+			$location.path("/");
+			
 		};
 	};
-   $http.get("/employees").then(function(response){
-       vm.test = response.data;
-   }).catch(function(err){
-       console.log(err);
-   })
-    
-}])
+	
+	EmployeeCtrl.$inject = ["$http", "authStatus"]
+	function EmployeeCtrl($http, authStatus) {
+		var vm = this;
+		vm.isLoggedIn = function() {
+			if (authStatus.isLoggedIn) {
+				return true;
+			} else {
+				return false;
+			};
+		};
+		$http.get("/employees").then(function(response){
+			vm.test = response.data;
+		}).catch(function(err){
+			console.log(err);
+		})
+		
+	}
+	
+})();
+
+
+
+
+
